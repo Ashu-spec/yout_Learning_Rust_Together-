d2a988dc3a43d7b0c38c4868307324944bd48d1e
Ashish Christian <christianashish050@gmail.com>
2022-11-25 12:54:49 +0530

    new

--- projects/Structs/src/main.rs
+++ projects/Structs/src/main.rs
@@ -1,3 +1,15 @@
+struct User {
+    active: bool,
+    username: &str,
+    email: &str,
+    sign_in_count: u64,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let user1 = User {
+        email: "someone@example.com",
+        username: "someusername123",
+        active: true,
+        sign_in_count: 1,
+    };
 }
